fa4227849c1e1b3c490ce508d97b9afc93c70aba
Kyle <e0031318@u.nus.edu>
2023-10-09 15:05:53 +0800

    feat: adds setup code for pulse modulation and frequency sweeps

--- qcore/instruments/drivers/anapico_apuasyn20.py
+++ qcore/instruments/drivers/anapico_apuasyn20.py
@@ -7,13 +7,10 @@ from qcore.instruments.instrument import Instrument, ConnectionError
 
 
 class APUASYN20(Instrument):
-    # Without the Fast Switching (FS) addon, the worse case switching speed is 500us. The typical speed is 200us.
-    WAIT_TIME = 500e-6
-
     def __init__(
         self,
         id: str,
-        channel: int = 0,
+        channel: int = 1,
         frequency: float = 0.0,
         phase: float = 0.0,
         power: float = 0.0,
@@ -64,6 +61,8 @@ class APUASYN20(Instrument):
     def channel(self, value: int) -> None:
         """Sets active channel"""
         self._handle.write(f":SEL {value}")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
 
     @property
     def frequency(self) -> float:
@@ -74,7 +73,8 @@ class APUASYN20(Instrument):
     def frequency(self, value: float) -> None:
         """Writes frequency in Hz"""
         self._handle.write(f":FREQ:CW {value}")
-        time.sleep(APUASYN20.WAIT_TIME)
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
 
     @property
     def phase(self) -> float:
@@ -85,7 +85,8 @@ class APUASYN20(Instrument):
     def phase(self, value: float):
         """Writes phase in rad"""
         self._handle.write(f":PHAS:ADJ {value}")
-        time.sleep(APUASYN20.WAIT_TIME)
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
 
     @property
     def power(self) -> float:
@@ -96,7 +97,8 @@ class APUASYN20(Instrument):
     def power(self, value: float):
         """Sets power in dBm"""
         self._handle.write(f":POW {value}")
-        time.sleep(APUASYN20.WAIT_TIME)
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
 
     @property
     def output(self) -> bool:
@@ -104,19 +106,78 @@ class APUASYN20(Instrument):
         return bool(self._handle.query(f"OUTP?"))
 
     @output.setter
-    def output(self, value: bool) -> None:
+    def output(self, value: bool):
         """ """
         if value:
             self._handle.write(f"OUTP ON")
         else:
             self._handle.write(f"OUTP OFF")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
 
-    def setup_pulse_mod(self) -> bool:
+    def get_channel_freq(self, channel: int) -> float:
+        """Returns freq of channel in Hz"""
+        return float(self._handle.query(f":SOUR {channel}:FREQ:CW?"))
+
+    def set_channel_freq(self, channel: int, value: float) -> float:
+        """Sets freq of channel in Hz"""
+        self._handle.write(f":SOUR {channel}:FREQ:CW {value}")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
+
+    def get_channel_pow(self, channel: int) -> float:
+        """Returns power of channel in dBm"""
+        return float(self._handle.query(f":SOUR {channel}:POW?"))
+
+    def set_channel_pow(self, channel: int, value: float) -> float:
+        """Sets power of channel in dBm"""
+        self._handle.write(f":SOUR {channel}:POW {value}")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
+
+    def get_channel_phase(self, channel: int) -> float:
+        """Returns phase of channel in rad"""
+        return float(self._handle.query(f":SOUR {channel}:PHAS?"))
+
+    def set_channel_phase(self, channel: int, value: float) -> float:
+        """Sets phase of channel in rad"""
+        self._handle.write(f":SOUR {channel}:PHAS {value}")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
+
+    def setup_pulse_mod(self, channel: int) -> bool:
         """ """
-        self._handle.write(f":PULM:SOUR EXT")
-        self._handle.write(f":PULM:STAT ON")
-        time.sleep(APUASYN20.WAIT_TIME)
-        pulse_mode_src = str(self._handle.query(":PULM:SOUR?"))
-        pulse_mode_on = bool(self._handle.query(":PULM:STAT?"))
+
+        # Sets reference osc to an external source (Rubidium clock)
+        self._handle.write(f":SOUR {channel}:ROSC:SOUR EXT")
+        # Make sure that freq change happens immediately and not on trigger
+        self._handle.write(f":SOUR {channel}:FREQ:TRIG OFF")
+        # Set all subsystems to fixed
+        self._handle.write(f":SOUR {channel}:FREQ:MODE CW")
+        self._handle.write(f":SOUR {channel}:POW:MODE CW")
+        self._handle.write(f":SOUR {channel}:PHASE:MODE CW")
+
+        # Turn off other modulation methods
+        self._handle.write(f":SOUR {channel}:AM:STAT OFF")
+        self._handle.write(f":SOUR {channel}:FM:STAT OFF")
+        self._handle.write(f":SOUR {channel}:PM:STAT OFF")
+
+        # Turn on pulse modulation and enable output
+        self._handle.write(f":SOUR {channel}:PULM:SOUR EXT")
+        self._handle.write(f":SOUR {channel}:PULM:STAT ON")
+        self._handle.write(f":OUTP {channel} ON")
+        self._handle.write(f":OUTP:BLAN {channel} OFF")
+        # Synchronize (wait until all previous commands have been executed completely)
+        self._handle.query("*OPC?")
+        pulse_mode_src = str(self._handle.query(f":SOUR {channel}:PULM:SOUR?"))
+        pulse_mode_on = bool(self._handle.query(f":SOUR {channel}:PULM:STAT?"))
 
         return (pulse_mode_src == "EXT") and pulse_mode_on
+
+    def setup_freq_sweep(self, channel: int, start: float, stop: float, step: float):
+        """ """
+        self._handle.write(f"SOUR {channel}:FREQ:MODE SWE")
+        self._handle.write(f"SOUR {channel}:FREQ:STAR {start}")
+        self._handle.write(f"SOUR {channel}:FREQ:STOP {stop}")
+        self._handle.write(f"SOUR {channel}:FREQ:STEP {step}")
+        self._handle.write(f":TRIG:TYPE POINT")
