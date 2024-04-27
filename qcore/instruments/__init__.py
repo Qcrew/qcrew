7033e21fccbbe4eaa27be9f1b8443a4434528845
qcrew-lab <general.qcre@gmail.com>
2024-04-27 15:22:31 +0800

    fix: fixes anapico import errors

--- qcore/instruments/__init__.py
+++ qcore/instruments/__init__.py
@@ -1,9 +1,10 @@
 """ """
 
-from qcore.instruments.drivers import GS200, LMS, MS46522B, QM, SA124, SC5503B, SC5511A
+from qcore.instruments.drivers import GS200, LMS, MS46522B, QM, SA124, SC5503B, SC5511A, APUASYN20
 from qcore.instruments.instrument import DummyInstrument
 
 __all__ = [
+    "APUASYN20",
     "GS200",
     "LMS",
     "MS46522B",
